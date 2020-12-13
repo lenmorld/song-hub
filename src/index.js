d47c2da0ffa370a4c87939c14ee0e01b9c799c66
Lenny LD <lenmorld@gmail.com>
2020-12-13 09:09:11 -0500

    fix DEV_MODE_CLIENT

--- src/index.js
+++ src/index.js
@@ -8,30 +8,28 @@ import Login from './Login'
 
 // allow webpack-dev-server to skip login
 // useful for just working on non-login related UI
-// const { DEV_MODE_CLIENT } = process.env
+const DEV_MODE_CLIENT = process.env.DEV_MODE_CLIENT === 'true'
 
-const DEV_MODE_USER = null
-
-// const DEV_MODE_USER = {
-//   display_name: 'Lenny',
-//   email: 'lenmorld@gmail.com',
-//   external_urls: {
-//     spotify: 'https://open.spotify.com/user/3wytubqe8gdcanop7i7hnzlom',
-//   },
-//   followers: { href: null, total: 6 },
-//   href: 'https://api.spotify.com/v1/users/3wytubqe8gdcanop7i7hnzlom',
-//   id: '3wytubqe8gdcanop7i7hnzlom',
-//   images: [
-//     {
-//       height: null,
-//       width: null,
-//       url:
-//         'https://scontent-atl3-1.xx.fbcdn.net/v/t31.0-1/p320x320/19787107_10209699780711449_8185009963243246747_o.jpg?_nc_cat=106&ccb=2&_nc_sid=0c64ff&_nc_ohc=DqI3FumxzL8AX_ceZSR&_nc_ht=scontent-atl3-1.xx&tp=6&oh=706626f35281c3c167d45d5dc0c6a11b&oe=5FF95DB4',
-//     },
-//   ],
-//   type: 'user',
-//   uri: 'spotify:user:3wytubqe8gdcanop7i7hnzlom',
-// }
+const DEV_MODE_USER = {
+  display_name: 'Lenny',
+  email: 'lenmorld@gmail.com',
+  external_urls: {
+    spotify: 'https://open.spotify.com/user/3wytubqe8gdcanop7i7hnzlom',
+  },
+  followers: { href: null, total: 6 },
+  href: 'https://api.spotify.com/v1/users/3wytubqe8gdcanop7i7hnzlom',
+  id: '3wytubqe8gdcanop7i7hnzlom',
+  images: [
+    {
+      height: null,
+      width: null,
+      url:
+        'https://scontent-atl3-1.xx.fbcdn.net/v/t31.0-1/p320x320/19787107_10209699780711449_8185009963243246747_o.jpg?_nc_cat=106&ccb=2&_nc_sid=0c64ff&_nc_ohc=DqI3FumxzL8AX_ceZSR&_nc_ht=scontent-atl3-1.xx&tp=6&oh=706626f35281c3c167d45d5dc0c6a11b&oe=5FF95DB4',
+    },
+  ],
+  type: 'user',
+  uri: 'spotify:user:3wytubqe8gdcanop7i7hnzlom',
+}
 
 // import './styles.css'
 
@@ -47,9 +45,9 @@ const App = () => {
     user = null // not logged in yet
   }
 
-  // if (DEV_MODE_CLIENT) {
-  // user = DEV_MODE_USER
-  // }
+  if (DEV_MODE_CLIENT && !user) {
+    user = DEV_MODE_USER
+  }
 
   // TODO: implement as context instead
   return (
